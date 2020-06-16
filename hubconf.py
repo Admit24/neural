c9baec325e7fa20aad7d972c2b2a1735dec62d8a
bernardomig <bernardo.lourenco@ua.pt>
2020-06-16 15:34:56 +0100

    updated hash for vgg model

--- hubconf.py
+++ hubconf.py
@@ -14,7 +14,7 @@ def vgg11(dataset='imagenet', pretrained=True):
         model = _vgg11(in_channels=3, num_classes=1000)
         if pretrained:
             state_dict = torch.hub.load_state_dict_from_url(
-                'http://files.deeplar.tk/neural/models/weights/classification/vgg/vgg11-imagenet-2c22bb86.pth',
+                'http://files.deeplar.tk/neural/models/weights/classification/vgg/vgg11-imagenet-c4ec67ec.pth',
                 check_hash=True)
             model.load_state_dict(state_dict)
         return model
