0531274b8ca560a95b76ea4a19cf9a0a49d58156
bernardomig <bernardo.lourenco@ua.pt>
2020-07-23 23:14:34 +0100

    added mdsr (edsr) and respective weights

--- neural/models/restauration/edsr.py
+++ neural/models/restauration/edsr.py
@@ -31,7 +31,7 @@ __all__ = [
 })
 def edsr(in_channels, out_channels, scale_factor):
     return EDSR(in_channels, out_channels, scale_factor,
-                num_channels=256, num_res_blocks=32, resblock_scaling=0.1)
+                width=256, num_blocks=32, resblock_scaling=0.1)
 
 
 @configure_model({
@@ -56,37 +56,55 @@ def edsr(in_channels, out_channels, scale_factor):
 })
 def edsr_baseline(in_channels, out_channels, scale_factor):
     return EDSR(in_channels, out_channels, scale_factor,
-                num_channels=64, num_res_blocks=16, resblock_scaling=None)
+                width=64, num_blocks=16, resblock_scaling=None)
+
+
+@configure_model({
+    'div2k': {
+        'in_channels': 3,
+        'out_channels': 3,
+        'scales': [2, 3, 4],
+        'state_dict': 'http://files.deeplar.tk/neural/weights/edsr/mdsr_baseline-div2k-c4070d35.pth',
+    }
+})
+def mdsr_baseline(in_channels, out_channels, scale_factors=[2, 3, 4]):
+    return MDSR(in_channels, out_channels, scale_factors=scale_factors, width=64, num_blocks=16)
+
+
+@configure_model({
+    'div2k': {
+        'in_channels': 3,
+        'out_channels': 3,
+        'scales': [2, 3, 4],
+        'state_dict': 'http://files.deeplar.tk/neural/weights/edsr/mdsr-div2k-50920243.pth',
+    }
+})
+def mdsr(in_channels, out_channels, scale_factors=[2, 3, 4]):
+    return MDSR(in_channels, out_channels, scale_factors=scale_factors, width=64, num_blocks=80)
 
 
 class EDSR(nn.Module):
 
     def __init__(self, in_channels, out_channels, scale_factor,
-                 num_channels=64,
-                 num_res_blocks=16,
+                 width=64,
+                 num_blocks=16,
                  resblock_scaling=None):
         super().__init__()
 
         self.head = nn.Conv2d(
-            in_channels, num_channels,
+            in_channels, width,
             kernel_size=3, padding=1)
-        self.features = nn.Sequential(*[
-            ResidualBlock(num_channels, num_channels, scaling=resblock_scaling)
-            for _ in range(num_res_blocks)
-        ] + [nn.Conv2d(num_channels, num_channels, 3, padding=1)])
-        if scale_factor in {2, 3}:
-            self.upsampling = Upsampling(num_channels, scale_factor)
-        elif scale_factor == 4:
-            self.upsampling = nn.Sequential(
-                Upsampling(num_channels, 2),
-                Upsampling(num_channels, 2),
-            )
-        else:
-            raise ValueError(
-                "scale_factor should be either 2, 3 or 4, "
-                "got {}".format(scale_factor))
+
+        layers = [
+            ResidualBlock(width, width, scaling=resblock_scaling)
+            for _ in range(num_blocks)
+        ] + [nn.Conv2d(width, width, 3, padding=1)]
+        self.features = nn.Sequential(*layers)
+
+        self.upsampling = create_upsampling(width, scale_factor)
+
         self.tail = nn.Conv2d(
-            num_channels, out_channels,
+            width, out_channels,
             kernel_size=3, padding=1)
 
     def forward(self, input):
@@ -97,19 +115,61 @@ class EDSR(nn.Module):
         return self.tail(x)
 
 
+class MDSR(nn.Module):
+    def __init__(self, in_channels, out_channels,
+                 scale_factors=[2, 3, 4],
+                 width=64,
+                 num_blocks=16,
+                 resblock_scaling=None):
+        super().__init__()
+
+        self.conv = nn.Conv2d(in_channels, width, 3, padding=1)
+
+        self.head = nn.ModuleDict({
+            str(scale_factor): nn.Sequential(
+                ResidualBlock(width, width, 5),
+                ResidualBlock(width, width, 5),
+            )
+            for scale_factor in scale_factors
+        })
+
+        layers = [
+            ResidualBlock(width, width, scaling=resblock_scaling)
+            for _ in range(num_blocks)
+        ] + [nn.Conv2d(width, width, 3, padding=1)]
+        self.features = nn.Sequential(*layers)
+
+        self.upsampling = nn.ModuleDict({
+            str(scale_factor): create_upsampling(width, scale_factor)
+            for scale_factor in scale_factors
+        })
+
+        self.tail = nn.Conv2d(width, out_channels, 3, padding=1)
+
+    def forward(self, input, scale_factor):
+        x = self.conv(input)
+        x = self.head[str(scale_factor)](x)
+        features = self.features(x)
+        x = x + features
+        x = self.upsampling[str(scale_factor)](x)
+        return self.tail(x)
+
+
 class ResidualBlock(nn.Module):
 
-    def __init__(self, in_channels, out_channels, scaling=1.0):
+    def __init__(self, in_channels, out_channels, kernel_size=3, scaling=1.0):
         super().__init__()
 
         self.scaling = scaling
 
         self.conv1 = nn.Conv2d(
             in_channels, in_channels,
-            kernel_size=3, padding=1)
+            kernel_size=kernel_size,
+            padding=kernel_size//2)
         self.conv2 = nn.Conv2d(
             in_channels, in_channels,
-            kernel_size=3, padding=1)
+            kernel_size=kernel_size,
+            padding=kernel_size//2)
 
     def forward(self, input):
         x = F.relu(self.conv1(input))
@@ -117,6 +177,20 @@ class ResidualBlock(nn.Module):
         if self.scaling is not None:
             x = self.scaling * x
         return input + x
+
+
+def create_upsampling(width, scale_factor):
+    if scale_factor in {2, 3}:
+        return Upsampling(width, scale_factor)
+    elif scale_factor == 4:
+        return nn.Sequential(
+            Upsampling(width, 2),
+            Upsampling(width, 2),
+        )
+    else:
+        raise ValueError(
+            "scale_factor should be either 2, 3 or 4, "
+            "got {}".format(scale_factor))
 
 
 class Upsampling(nn.Module):
