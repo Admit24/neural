43130f3bcd5df047dec20a0f8e9ef17ffa6d513a
bernardomig <bernardo.lourenco@ua.pt>
2020-07-31 13:37:46 +0100

    updated enet weights

--- neural/models/segmentation/enet.py
+++ neural/models/segmentation/enet.py
@@ -13,7 +13,7 @@ __all__ = ['ENet', 'enet']
     'cityscapes': {
         'in_channels': 3,
         'out_channels': 19,
-        'state_dict': 'http://files.deeplar.tk/neural/weights/enet/enet-cityscapes-db32303b.pth',
+        'state_dict': 'http://files.deeplar.tk/neural/weights/enet/enet-cityscapes-fa67bff1.pth',
     }
 })
 def enet(in_channels=3, out_channels=19):
